d4c7166669ab0c7c6d90f19440be23940868c826
Sergey Levchenko <bossi4ek@gmail.com>
2013-11-27 00:18:24 +0200

    Added tests

--- src/Sf2films/FilmsBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Sf2films/FilmsBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,33 @@
 namespace Sf2films\FilmsBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Sf2films\FilmsBundle\Repository\ContentRepository;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+//================================================================
+    public function transliterData()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        return array(
+            array(array('притет мир')),
+            array(array('123')),
+            array(array('hello world'))
+        );
     }
+
+//================================================================
+    /**
+     * @dataProvider transliterData
+     */
+    public function testTransliter($data) {
+        $transliter = $this->getMockBuilder('Sf2films\FilmsBundle\Transliter')
+//            ->disableOriginalConstructor()
+            ->setMethods(null)
+            ->getMock()
+        ;
+
+        $result = $transliter->getTranslit($data[0]);
+        $this->assertTrue($result != '');
+    }
+
 }
